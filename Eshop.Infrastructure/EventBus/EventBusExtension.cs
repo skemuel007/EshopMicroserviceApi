63e2078babd05d17d738beeed58cc5a9ff1a0a7e
Stanley-Kemuel Lloyd Salvation <skemuel007@gmail.com>
2023-12-30 16:05:50 +0100

    RabbitMQ masstransit configuration complete

---
+++ Eshop.Infrastructure/EventBus/EventBusExtension.cs
@@ -0,0 +1,39 @@
+using MassTransit;
+using MassTransit.MultiBus;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Eshop.Infrastructure.EventBus;
+
+public static class EventBusExtension
+{
+    public static IServiceCollection AddRabbitMq(this IServiceCollection services, IConfiguration configuration)
+    {
+        var rabbitMq = new RabbitMqOption();
+        configuration.GetSection("RabbitMq").Bind(rabbitMq);
+
+        services.AddMassTransit(x =>
+        {
+            // https://medium.com/@bantyder/how-to-use-masstransit-8-0-13-rabbitmq-with-aspnetcore-7e199998b92d
+            x.UsingRabbitMq((ctx,cfg)=>
+            { 
+                cfg.Host(new Uri(rabbitMq.ConnectionString),"/" , hostCfg => 
+                { 
+                    hostCfg.Username(rabbitMq.Username);
+                    hostCfg.Password(rabbitMq.Password); 
+                }); 
+                cfg.ConfigureEndpoints(ctx); 
+            });
+        });
+
+        return services;
+    }
+    
+}
+
+public class RabbitMqOption
+{
+    public string ConnectionString { get; set; }
+    public string Username { get; set; }
+    public string Password { get; set; }
+}
