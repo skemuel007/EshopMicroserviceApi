5588f75c430adf6dd35a00aaf96f5d816baca5b8
Stanley-Kemuel Lloyd Salvation <skemuel007@gmail.com>
2023-12-30 14:01:28 +0100

    Create Product Service and Repository

--- Eshop.Product.Api/Program.cs
+++ Eshop.Product.Api/Program.cs
@@ -1,9 +1,14 @@
+using Eshop.Infrastructure.Mongo;
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddControllers();
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddMongoDb(builder.Configuration);
 
 var app = builder.Build();
 
@@ -15,5 +20,9 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
 
 app.Run();
